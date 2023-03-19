7658df95ce7757643a1befff4c075f4084c5c942
Marc Duquesnoy <marc.duquesnoy@obspm.fr>
2023-03-19 19:48:20 +0100

    je sais plus

--- models/contract.py
+++ models/contract.py
@@ -33,26 +33,20 @@ class Contract(db.Model):
 
     users: orm.Mapped[List["User"]] = orm.relationship(secondary = association_table, back_populates = "contracts")
     
-    
     def set_password(self, password):
         self.contract_password = bcrypt.generate_password_hash(password).decode("utf-8")
-        
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.contract_password, password)
     
-    
     def set_identifiant(self, identifiant):
         self.contract_identifiant = bcrypt.generate_password_hash(identifiant).decode("utf-8")
 
-
     def check_identifiant(self, identifiant):
         return bcrypt.check_password_hash(self.contract_identifiant, identifiant)
-
     
     def set_num(self, num):
         self.contract_num = bcrypt.generate_password_hash(num).decode("utf-8")
-
 
     def check_num(self, num):
         return bcrypt.check_password_hash(self.contract_num, num)

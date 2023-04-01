2240f872c17d4ca90e59bf99da61735fd1004a33
marcduquesnoyobspm <marclucien.duquesnoy@gmail.com>
2023-04-02 00:37:12 +0200

    changement dynamique du nom dans overview

--- __init__.py
+++ __init__.py
@@ -1,34 +1,36 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_moment import Moment
+from flask_talisman import Talisman
 from .controllers import login_manager, controllers, init_controllers
 from .models import db, bcrypt
 from .utils.config import Config
 
 
 def create_app(config_class=Config):
-
     app = Flask(__name__, instance_relative_config=True)
 
     app.config.from_object(config_class)
 
     migrate = Migrate()
     moment = Moment()
+    # talisman = Talisman()
 
     db.init_app(app)
     migrate.init_app(app, db)
     moment.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
+    # talisman.init_app(app, content_security_policy=None)
 
     init_controllers()
-    
+
     app.register_blueprint(controllers)
 
     app.config.update(
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax',
+        SESSION_COOKIE_SECURE=True,
+        SESSION_COOKIE_HTTPONLY=True,
+        SESSION_COOKIE_SAMESITE="Lax",
     )
 
     return app
--- controllers/overview.py
+++ controllers/overview.py
@@ -1,45 +1,46 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user, login_user, logout_user
 from ..utils.forms import OverviewUpdateProfileForm
 from ..models import db
+from ..models.user import User
 
 
-overview = Blueprint('overview', __name__)
+overview = Blueprint("overview", __name__)
 
 
-@overview.route('/overview')
+@overview.route("/overview")
 @login_required
 def overview_page():
-    
     form = OverviewUpdateProfileForm()
-    
-    tab = request.args.get('tab')
-    
-    if tab is None:
-    
-        tab = "overview"
-    
-    return render_template('overview.html', form = form, tab = tab)
-    
 
-@overview.route('/overview/update/profile', methods=["POST"])
+    tab = request.args.get("tab")
+
+    if tab is None:
+        tab = "overview"
+
+    return render_template("overview.html", form=form, tab=tab)
+
+
+@overview.route("/overview/update/profile", methods=["POST"])
 @login_required
 def overview_update_profile():
-    
     form = OverviewUpdateProfileForm()
-    
+
     if form.validate_on_submit():
-        
         current_user.user_firstname = form.first_name.data
-        
+
         current_user.user_lastname = form.last_name.data
-        
+
         db.session.commit()
-        
-        User.query.get(int(id))
-        
-        login_user(current_user)
-        
-        return {"success":True}
-    
-    return {"success":False}
+
+        modified_user = User.query.get(current_user.id)
+
+        login_user(modified_user)
+
+        return {
+            "success": True,
+            "first_name": current_user.user_firstname,
+            "last_name": current_user.user_lastname,
+        }
+
+    return {"success": False}
